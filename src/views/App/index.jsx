d4781c375dd33c63b7d38a3e049a94006781ff12
Facu Velasco <velascofacundo@gmail.com>
2021-02-20 14:05:58 -0300

    header

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,12 +1,31 @@
 import * as React from "react";
+import axios from "axios";
 
 import Header from "../Header";
 import Content from "../Content";
 
 export default function App() {
+  const [user, setUser] = React.useState({});
+
+  const sendLoginRequest = () => {
+    // fetch fake user
+    axios
+      .post("/api/login")
+      .then(({ data }) => {
+        setUser(data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  // fetch flights
+
+  // fetch favs?
+
   return (
     <>
-      <Header />
+      <Header user={user} handleLoginClick={sendLoginRequest} />
       <Content />
     </>
   );
--- src/views/Header/index.jsx
+++ src/views/Header/index.jsx
@@ -1,14 +1,30 @@
 import * as React from "react";
 
-import Filters from "./Filters";
+import { Button, PageHeader, Avatar, Image } from "antd";
+
+import { FaPlane } from "react-icons/fa";
 
 import s from "./style.module.scss";
 
-export default function Header() {
+export default function Header({ user, handleLoginClick }) {
   return (
     <header>
-      <h3>Aerostates</h3>
-      <Filters />
+      <PageHeader
+        className="site-page-header"
+        avatar={{ icon: <FaPlane /> }}
+        title="Aerostates"
+        subTitle="Flights and state managment"
+      />
+      {user ? (
+        <div className={s.user}>
+          <p>Welcome {user.name}!</p>
+          <Avatar src={<Image src={user.img} />} />
+        </div>
+      ) : (
+        <Button type="primary" size="large" onClick={handleLoginClick}>
+          Login
+        </Button>
+      )}
     </header>
   );
 }
